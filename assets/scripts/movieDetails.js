234ee2cf7694865cfb0db3daf2254270a1de9599
Ethan Wilson <ethanwilson1991@gmail.com>
2021-02-08 19:17:40 -0600

    hamburger anim changes, header h1 media query

--- assets/scripts/movieDetails.js
+++ assets/scripts/movieDetails.js
@@ -3,7 +3,15 @@
 document.addEventListener("DOMContentLoaded", buildPage);
 var hamburger = document.querySelector('.hamburger');
 var menu = document.querySelector('.main-menu');
+var menuGroup = document.querySelector('.menu-group');
 hamburger.addEventListener("click", hamburgerChecked);
+menu.addEventListener("transitionend", () => {
+  if (menu.classList.contains('active')) {
+    menuGroup.style.visibility = "visible";
+  } else {
+    menuGroup.style.visibility = "hidden";
+  }
+})
 
 function hamburgerChecked () {
   menu.classList.toggle('active');
